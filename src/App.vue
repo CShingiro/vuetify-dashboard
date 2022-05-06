efd16cc09a70c1095027f43dada9794bc64cceea
CShingiro <nemyent@gmail.com>
2022-05-05 20:57:15 -0400

    Updates to include DashBoard

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,19 @@
         Vuetify Dashboard
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
     <v-content>
       <!-- Login Module -->
-      <v-card width="400" class="mx-auto mt-5">
-      <v-card-title></v-card-title>
-      <h1 class="display-1">Login</h1>
-      <v-card-text>
-        <v-form>
-          <v-text-field
-            label="Username"
-            prepend-icon="mdi-account-circle"
-          />
-          <v-text-field
-            :type="showPassword ? 'text' : 'password'"
-            label="Password"
-            prepend-icon="mdi-lock"
-            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-            @click:append="showPassword = !showPassword"
-          />
-        </v-form>
-      </v-card-text>
-      <v-divider></v-divider>
-      <v-card-action>
-        <v-btn color="success">Register</v-btn>
-        <v-spacer></v-spacer>
-        <v-btn color="info">Login</v-btn>
-      </v-card-action>
-      </v-card>
+      <router-view></router-view>
     </v-content>
     <v-footer
       color="primary lighten-1"
@@ -46,13 +29,14 @@
       >
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-flex
           primary
@@ -76,10 +60,15 @@ export default {
 
 
   data: () => ({
-    showPassword: false,
     links: [
-      "Home",
-      "Login"
+      {
+        label: "Home",
+        url: "/"
+      },
+      {
+        label: "Login",
+        url: "/login"
+      }
     ]
   }),
 }
